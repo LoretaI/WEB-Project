143a8583e9aae351e734f15135b9a99a1a244f27
dalmasmaka <93519957+dalmasmaka@users.noreply.github.com>
2022-03-11 17:27:58 -0800

    Pjesa e JavaScript per sliderin e img

--- js/projects.js
+++ js/projects.js
@@ -1,25 +1,46 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header.container');
-
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
-});
-
-document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
-});
-
-menu_item.forEach((item) => {
-	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
-	});
-});
+const first = () => {
+    document,
+    getElementbyId('slide').src = 'IMG/foto1.jpg';
+}
+setInterval(first, 2000);
+const second = () => {
+    document.getElementById('slide').src = 'IMG/foto2.jpg';
+}
+setInterval(second, 4000);
+const third = () => {
+    document,
+    getElementbyId('slide').src = 'IMG/foto3.jpg';
+}
+setInterval(third, 6000);
+const fourth = () => {
+    document.getElementById('slide').src = 'IMG/foto4.jpg';
+}
+setInterval(fourth, 8000);
+const fifth = () => {
+    document,
+    getElementbyId('slide').src = 'IMG/foto5.jpg';
+}
+setInterval(fifth, 10000);
+const sixth = () => {
+    document.getElementById('slide').src = 'IMG/foto6.jpg';
+}
+setInterval(sixth, 12000);
+const seventh = () => {
+    document,
+    getElementbyId('slide').src = 'IMG/foto7.jpg';
+}
+setInterval(seventh, 14000);
+const eighth = () => {
+    document.getElementById('slide').src = 'IMG/foto8.jpg';
+}
+setInterval(eighth, 16000);
+const ninth = () => {
+    document,
+    getElementbyId('slide').src = 'IMG/foto9.jpg';
+}
+setInterval(ninth, 18000);
+const tenth = () => {
+    document.getElementById('slide').src = 'IMG/foto10.jpg';
+}
+setInterval(tenth, 20000);
+//projects
